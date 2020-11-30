ae5bacd9d850e137dba080ccf7517c19ff7d66ee
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-30 22:07:29 +0100

    mid-video push

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
-import { Card } from 'lib/Card'
+import { Card } from './lib/Card'
 
 const App = () => {
   return (
    <div>
-     <Card />
+     <Card 
+      title="I am the title" 
+      secondaryText="I'm a fancy paragraph"
+     />
    </div>
   )
 }
--- code/src/lib/Card.js
+++ code/src/lib/Card.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 
-export const Card = () => {
-  <div>
-    Here's my card!
-  </div>
+const Container = styled.div`
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0,0,0,.12);
+  padding: 20px;
+  border-radius: 3px;
+  background: #fff;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 24px;
+`
+
+const SecondaryText = styled.p`
+  margin: 0;
+  color: #6b6b6b;
+`
+
+const Thumbnail = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+`
+
+export const Card = ({ title, secondaryText, thumbnailURL }) => {
+
+  return (
+    <Container>
+      <Thumbnail url={thumbnailURL} />
+      {title && <Title>{title}</Title>}
+        {secondaryText && <SecondaryText>{secondaryText}</SecondaryText>}
+    </Container>
+  )
 }
