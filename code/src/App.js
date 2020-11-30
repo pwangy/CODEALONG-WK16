d3d64483cb01cbf26918848062ef6109a6e125a5
Peggy <64912576+pwangy@users.noreply.github.com>
2020-11-30 23:28:57 +0100

    end of codealong

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 import { Card } from './lib/Card'
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`
 
 const App = () => {
   return (
    <div>
-     <Card 
+     <ThinnerCard 
+      coverImage="https://www.fillmurray.com/500/300"
+      thumbnailURL="https://www.fillmurray.com/100/100"
       title="I am the title" 
       secondaryText="I'm a fancy paragraph"
      />
+
+     <ThinnerCard title="I have no other props sad-face" secondaryText="HEY! Whut am I? Chopped liver?">
+       Don't forget the children!
+     </ThinnerCard>
+
+     <Card title="Hay is for horses"/>
+
+     <Card>
+       Hello! I'm a child.
+     </Card>
    </div>
   )
 }
--- code/src/lib/Card.js
+++ code/src/lib/Card.js
@@ -3,9 +3,9 @@ import styled from 'styled-components/macro'
 
 const Container = styled.div`
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0,0,0,.12);
-  padding: 20px;
   border-radius: 3px;
   background: #fff;
+  margin-bottom: 20px;
 `
 
 const Title = styled.h1`
@@ -22,15 +22,46 @@ const Thumbnail = styled.div`
   width: 70px;
   height: 70px;
   border-radius: 50%;
+  background-image: url(${(props) => props.url });
+  background-size: cover;
+  margin-right: 16px;
+`
+const TitleBar = styled.div`
+  display: flex;
+  align-items: center;
 `
 
-export const Card = ({ title, secondaryText, thumbnailURL }) => {
+const CoverImage = styled.img`
+  width: 100%;
+  border-radius: 3px 3px 0 0;
+`
+
+const Content = styled.div`
+  padding 20px;
+`
+
+const ChildreContent = styled.div`
+  background: papayawhip;
+  color: palevioletred;
+  padding: 10px;
+`
+
+export const Card = ({ title, secondaryText, thumbnailURL, coverImage, className, children }) => {
 
   return (
-    <Container>
-      <Thumbnail url={thumbnailURL} />
-      {title && <Title>{title}</Title>}
-        {secondaryText && <SecondaryText>{secondaryText}</SecondaryText>}
+    <Container className={className}>
+    {coverImage && <CoverImage src={coverImage} />}
+      <Content>
+        <TitleBar>
+          {thumbnailURL &&  <Thumbnail url={thumbnailURL} />} 
+          <div>
+            {title && <Title>{title}</Title>}
+            {secondaryText && <SecondaryText>{secondaryText}</SecondaryText>}
+          </div>
+        </TitleBar>
+
+        {children && <ChildreContent>{children}</ChildreContent>}
+      </Content>
     </Container>
   )
 }
